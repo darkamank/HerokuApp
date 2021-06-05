1020df324616d1b76ede490e16d636469d5186c2
Anton Sidorik <darkmank79@gmail.com>
2021-06-05 17:09:02 +0300

    Add test which chech notifications text

---
+++ src/test/java/NotificationMessages.java
@@ -0,0 +1,17 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class NotificationMessages extends Base {
+
+    @Test
+    public void checkNotification(){
+        driver.get("http://the-internet.herokuapp.com/notification_message_rendered");
+        WebElement button = driver.findElement(By.xpath("//a[text()='Click here']"));
+        button.click();
+        String flashNotice = driver.findElement(By.xpath("//div[@class='flash notice']")).getText();
+     //   System.out.println(flashNotice);
+        Assert.assertEquals(flashNotice,"Action successful"+"\n"+"×","Action unsuccessful");
+    }
+}
