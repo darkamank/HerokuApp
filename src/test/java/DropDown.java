de6e9e785945b8344befbb89666a73f5cb191279
Anton Sidorik <darkmank79@gmail.com>
2021-06-05 08:20:54 +0300

    Add test DropDown

---
+++ src/test/java/DropDown.java
@@ -0,0 +1,19 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class DropDown extends Base{
+
+    @Test
+    public void setValue (){
+        driver.get("http://the-internet.herokuapp.com/dropdown");
+        Select dropDown = new Select(driver.findElement(By.id("dropdown")));
+        dropDown.selectByValue("1");
+        String nameFirstElement = dropDown.getFirstSelectedOption().getText();
+        Assert.assertEquals(nameFirstElement,"Option 1", "The first element didn't select");
+        dropDown.selectByValue("2");
+        String nameSecondElement = dropDown.getFirstSelectedOption().getText();
+        Assert.assertEquals(nameSecondElement,"Option 2", "The second element didn't select");
+    }
+}
